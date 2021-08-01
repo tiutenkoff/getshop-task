ce21d95ed257f1d5c1d723f77bb52f65b1ba49c9
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2021-08-01 15:40:09 +0700

    Layout a page with a promo and banner

--- src/pages/Banner.tsx
+++ src/pages/Banner.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/banner.scss';
+import { NavLink} from 'react-router-dom'
 
 export const Banner = () => {
     return (
         <div className="banner">
-            
+            <img className="banner__image banner__image--QR" src="images/banner/banner.png" alt="" />
+            <img className="banner__image" src="images/banner/video.png" alt="" />
+            <NavLink to="/main">
+                <img className="banner__image banner__image--BTN" src="images/banner/btn.png" alt="" />
+            </NavLink>
         </div>
     )
 }
