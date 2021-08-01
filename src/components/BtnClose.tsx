a4a33f8556748143a7fbd916e9f527f297cb9b98
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2021-08-01 21:13:18 +0700

    Created files for the component of the close buttons and for the code with the signature, as well as styles for them

---
+++ src/components/BtnClose.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/btnClose.scss';
+
+export const BtnClose = () => {
+    return (
+        <div className="btn-block">
+
+        </div>
+    )
+}
---
+++ src/components/QRcode.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/qrcode.scss';
+
+export const QRcode = () => {
+    return (
+        <div className="qr-code">
+
+        </div>
+    )
+}
