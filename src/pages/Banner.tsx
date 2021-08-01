52e802d55f2e76fda9df84d4de8bb538784d0753
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2021-08-01 15:36:49 +0700

    Created a project, cleaned it of unnecessary files, split the project into files of components, pages, styles, connected routing, and deployed a template in each page, connected a style file to them

---
+++ src/pages/Banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/banner.scss';
+
+export const Banner = () => {
+    return (
+        <div className="banner">
+            
+        </div>
+    )
+}
---
+++ src/pages/Final.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/final.scss';
+
+export const Final = () => {
+    return (
+        <div className="final">
+            
+        </div>
+    )
+}
