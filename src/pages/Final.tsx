2570e001f4f91f45abc4c9c6804f8b5c31e9269c
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2021-08-01 21:16:58 +0700

    Made up the final page

--- src/pages/Final.tsx
+++ src/pages/Final.tsx
@@ -4,7 +4,22 @@ import '../styles/final.scss';
 export const Final = () => {
     return (
         <div className="final">
-            
+            <div className="final__left">
+                <div className="final__success">
+                    <div className="final__success--center">
+                        <h1 className="left__title">Заявка принята</h1>
+                        <p className="left__text">Держите телефон под рукой.<br />Скоро с Вами свяжется наш менеджер. </p>
+                    </div>
+                </div>
+            </div>
+        
+            <div className="final__right">
+                <img src="images/final/wallpaper.png" alt="" />
+                <div className="right__up">
+                </div>
+                <div className="right__down">
+                </div>
+            </div>
         </div>
     )
 };
