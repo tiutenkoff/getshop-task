8ebf4e72450eb87b0618f934ef810972b8183f15
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2021-08-01 21:21:26 +0700

    Made up the qr-code component and placed it on the final page

--- src/components/QRcode.tsx
+++ src/components/QRcode.tsx
@@ -4,7 +4,8 @@ import '../styles/qrcode.scss';
 export const QRcode = () => {
     return (
         <div className="qr-code">
-
+            <p className="qr-code__text">Сканируйте QR-код для получения дополнительной информации</p>
+            <img className="qr-code__img" src="images/final/qrcode.png" alt="" />
         </div>
     )
 }
