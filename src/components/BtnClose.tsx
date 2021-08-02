bfc61b06372f41e346ee3084ae4abcb9e93ed07e
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2021-08-02 17:04:53 +0700

    Posted a link to return to the page with a banner on BtnClose

--- src/components/BtnClose.tsx
+++ src/components/BtnClose.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
+
 import '../styles/btnClose.scss';
 
 export const BtnClose = () => {
     return (
-        <div className="btn-block">
-            <button className="btn-close">
-                <div className="btn-close__lines">
-                    <span className="btn-close--lineFirst"></span>
-                    <span className="btn-close--lineSecond"></span>
-                </div>
-            </button>
-        </div>
+        <NavLink to="/">
+            <div className="btn-block">
+                <button className="btn-close">
+                    <div className="btn-close__lines">
+                        <span className="btn-close--lineFirst"></span>
+                        <span className="btn-close--lineSecond"></span>
+                    </div>
+                </button>
+            </div>
+        </NavLink>
     )
 };
