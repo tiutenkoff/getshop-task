90566a45b115116892f07f4f28e0501df44fe9eb
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2021-08-03 14:47:41 +0700

    I inserted a video instead of a picture and made the banner retractable

--- src/pages/Banner.tsx
+++ src/pages/Banner.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import '../styles/banner.scss';
 import { NavLink} from 'react-router-dom'
 
-export const Banner = () => {
+export const Banner: React.FC = () => {
+    const [showBanner, setShowbanner] = React.useState(false);
+
+    React.useEffect(() => {
+        setBanner();
+    }, [])
+
+    const setBanner = () => {
+        setTimeout(() => {
+            setShowbanner(true);
+        }, 5000)
+    };
+
     return (
         <div className="banner">
-            <div className="banner__next">
+            <div className={`banner__next ${showBanner ? 'banner__next--show' : ''}`}>
                 <h3 className="banner__title">Исполните мечту вашего малыша!<br/>Подарите ему собаку!</h3>
                 <img src="images/final/qrcode.png" alt="" />
                 <p className="banner__text">Сканируйте QR-код<br/> или нажмите ОК</p>
@@ -13,7 +25,10 @@ export const Banner = () => {
                     <button className="banner__btn">ОК</button>
                 </NavLink>
             </div>
-            <img className="banner__video" src="images/banner/video.png" alt="" />
+            <video width="1280" height="720" autoPlay muted >
+                <source src="videos/videobanner.mp4"/>
+            </video>
+            <img className="banner__back" src="images/banner/video.png" alt="" />
         </div>
     )
 };
